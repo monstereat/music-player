2734296f4fb646afbb6d31d0cdd91a005f023fbb
Monstereat <532160108@qq.com>
2018-11-26 09:21:15 +0800

    歌词显示与播放进度同步高亮（高度进度慢了点看详细介绍）
    增加一个工具库，方便以后模块化改进，歌词与播放进度同步高亮（歌词文件中的时间进度比歌曲时间要晚点所以高亮效果上慢了点）

--- tools.js
+++ tools.js
@@ -1,9 +1,47 @@
-var e = function(name){
-    var ele = document.querySelector(name)
-    return ele
+var log = function() {
+    return console.apply(console, arguments)
 }
+
+var e = function(name) {
+    return document.querySelector(name)
+}
+
+var es = function(name) {
+    return document.querySelectorAll(name)
+}
+
 var bindEvent = function(element, event) {
-    element.addEventListener(event){
-        
+    element.addEventListener(event, callback)
+}
+
+var appendHtml = function(element, html) {
+    element.insertAdjacentHTML('beforeend', html)
+}
+
+var toggleClass = function(element, className) {
+    if(element.classList.contains(className)) {
+        element.classList.removeClass(className)
+    } else {
+        element.classList.add(className)
     }
+}
+
+var removeClassAll = function(className) {
+    var selector = '.' + className
+    var elements = document.querySelectorAll(className)
+    for(var i = 0; i < elements.length; i++) {
+        elements[i].removeClass(selector)
+    }
+}
+
+var bindAll = function(selector, eventName, callback) {
+    var elements = document.querySelectorAll(selector)
+    for(let i = 0; i < elements.length; i++) {
+        bindEvent(elements[i], eventName, callback)
+    }
+}
+
+// 查找 element 的所有值元素
+var findChild = function(element, selector) {
+    element.querySelector(selector)
 }
